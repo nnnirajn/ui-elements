441e6c2f2e3ea932f8b90f96d0c2deec0e033336
Niraj Narkhede <narkhede400@gmail.com>
2025-04-08 08:42:25 +0530

    Add files via upload

---
+++ jQuery Elements/AOS/script.js
@@ -0,0 +1,11 @@
+AOS.init({
+  duration: 1000,
+    once: true // for making only one time animation
+});
+
+   // AOS.init({
+   //    offset: 200,
+   //    duration: 600,
+   //    easing: 'ease-in-sine',
+   //    delay: 100,
+   //  });
